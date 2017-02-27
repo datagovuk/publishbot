4de593dc4732f2e993b95edb5cea5eaaeb69ac1b
Ross Jones <ross@servercode.co.uk>
2017-02-27 13:09:02 +0000

    Web views for prototype

--- main.go
+++ main.go
@@ -1,11 +1,61 @@
 package main
 
-import "fmt"
-import "github.com/datagovuk/publishbot/adapters"
+import (
+	"crypto/subtle"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func BasicAuth(handler http.HandlerFunc, username string, password string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Please enter auth details"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorised.\n"))
+			return
+		}
+		handler(w, r)
+	}
+}
 
 func main() {
 
-	fmt.Println("Publishbot")
-	adapters := adapters.List()
-	fmt.Printf("%v\n", adapters)
+	username := os.Getenv("HTTP_USERNAME")
+	password := os.Getenv("HTTP_PASSWORD")
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/status", BasicAuth(status, username, password))
+	http.HandleFunc("/preview", BasicAuth(preview, username, password))
+	http.HandleFunc("/", BasicAuth(homepage, username, password))
+
+	log.Println("Listening...")
+	http.ListenAndServe("127.0.0.1:3000", nil)
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	tmpl := get_template("index.html")
+	tmpl.ExecuteTemplate(w, "layout", nil)
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	tmpl := get_template("status.html")
+	tmpl.ExecuteTemplate(w, "layout", nil)
+}
+
+func preview(w http.ResponseWriter, r *http.Request) {
+	tmpl := get_template("preview.html")
+	tmpl.ExecuteTemplate(w, "layout", nil)
+}
+
+func get_template(name string) *template.Template {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", name)
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	return tmpl
 }
