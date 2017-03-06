5ad7fbea085bb6a01939d19e2cf395ea1ba9d927
Ross Jones <ross@servercode.co.uk>
2017-03-06 16:46:50 +0000

    Watch the configured folder for new files

--- adapters.go
+++ adapters.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
 
 func RunAdapter(config AdapterConfig) {
 	log.Println("Running", config.Title)
@@ -12,5 +16,30 @@ func RunAdapter(config AdapterConfig) {
 }
 
 func directoryAdapter(config AdapterConfig) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
 
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(config.Arguments["folder"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 }
